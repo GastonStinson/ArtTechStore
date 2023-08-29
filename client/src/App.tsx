e17b562fc8ef0132ca348cc1797e430e0a5c3acb
FelixGitHu <132509492+FelixGitHu@users.noreply.github.com>
2023-08-29 18:11:53 -0300

    Agregar el react-router-dom y Las Rutas del APP
    Aca se agrego el react-router-dom y las posibles rutas con un login y un logout en App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,33 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
+import { Routes,Route, useLocation, useNavigate,} from "react-router-dom";
+import axios from 'axios';
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-
+  const location = useLocation();
+  const navigate = useNavigate();
+  const [access, setAccess] =useState(false);
+  function logout() {
+    setAccess(false);
+  }
+  function login(userData) {
+  const { email, password } = userData;
+  const URL = 'http://localhost:3001/user/login/';
+  axios(URL + `?email=${email}&password=${password}`).then(({ data }) => {
+    const { access } = data;
+    setAccess(access);
+    access && navigate('/Landing');
+  });
+    }
+  useEffect(() => {!access && navigate("/");
+   }, [access]);
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {(location.pathname !== "/" && location.pathname !== "/Landing") && (<Nav out={logout}/>)}
+      <Routes>
+        <Route path='/' element={<Login login={login}/>}/>
+        <Route path='/landing'element={<LandingPage/>}/>
+        <Route path='/home' element={<Home/>}/>
+        <Route path='/create' element={<CreateForm/>}/>
+        <Route path='*' element={<div>This is the wrong page</div>}/>
+        <Route path='/detail' element={<Detail/>}/>
+      </Routes>
     </>
   )
 }
