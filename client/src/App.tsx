102ac82a005aa81995a9c7401636e7744dab02cd
YamilaHorn <hornmacarena12@hotmail.com>
2023-08-30 14:37:57 -0300

    diseño de Card

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useLocation, Route, Routes } from 'react-router-dom'; // Añade Route y Routes a tus importaciones
+import Card from './Card/Card';
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App: React.FC = () => {
+  const location = useLocation();
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  if (location.pathname === '/') {
+    const cardProps = {
+      imageURL: 'arte.jpg',
+      type: 'Type',
+      name: 'Name',
+      artistName: 'Artist Name',
+      cost: '$'
+    };
+
+    return (
+      <>
+        <Routes>
+          <Route path="/" element={<Card {...cardProps} />} />
+        </Routes>
+      </>
+    );
+  }
 }
 
-export default App
+export default App;
